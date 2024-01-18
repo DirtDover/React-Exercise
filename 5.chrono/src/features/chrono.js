15eb4802b3d5a0e50d8992aec2b4573a18a89e10
DirtDover <benjaminmbureau@gmail.com>
2024-01-18 17:38:29 +0100

    Mise en place du tick.

--- 5.chrono/src/features/chrono.js
+++ 5.chrono/src/features/chrono.js
@@ -32,8 +32,8 @@ export const chrono = createSlice({
 
                 if(!state.isPlaying) {
                     chosenState.value = chosenState.value + action.payload.value
-                    chosenState.runningValue = chosenState.value + action.payload.value
-                    state.displayedValue.value = chosenState.value
+                    chosenState.runningValue = chosenState.runningValue + action.payload.value
+                    state.displayedValue.value = chosenState.runningValue
                 }
                 else {
                     chosenState.value = chosenState.value + action.payload.value
@@ -44,7 +44,22 @@ export const chrono = createSlice({
             }
         },
         tick: (state, action) => {
-            console.log("tick");
+            if(state.session.runningValue > 0) {
+                state.session.runningValue--
+                state.displayedValue.value = state.session.runningValue
+                state.displayedValue.heading = "Work"
+            }
+            else if (state.pause.runningValue > 0){
+                state.pause.runningValue--
+                state.displayedValue.value = state.pause.runningValue
+                state.displayedValue.heading = "Pause"
+            } else {
+              state.cycles++
+              state.session.runningValue = state.session.value - 1  
+              state.displayedValue.value = state.session.value - 1
+              state.displayedValue.heading = "Work"
+              state.pause.runningValue = state.pause.value  
+            }
         },
         setUpChrono: (state, action) => {
             state.isPlaying = true
@@ -53,6 +68,10 @@ export const chrono = createSlice({
         resetChrono: (state, action) => {
             window.clearInterval(state.intervalID)
             state.isPlaying = false
+            state.session.runningValue = state.session.value
+            state.pause.runningValue = state.pause.value
+            state.displayedValue.value = state.session.runningValue
+            state.cycles = 0
         }
     }
 })
