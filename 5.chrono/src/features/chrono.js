3b15a7c6ab53f7f16186d050265aeeff6738ab5f
DirtDover <benjaminmbureau@gmail.com>
2024-01-18 16:57:29 +0100

    Mise en place des btns + et -

--- 5.chrono/src/features/chrono.js
+++ 5.chrono/src/features/chrono.js
@@ -22,8 +22,29 @@ export const chrono = createSlice({
     name: "chrono",
     initialState,
     reducers: {
+        updateChronoValues: (state, action) => {
+            const chosenState = state[action.payload.type]
+           
+            // on bloque en dessous de 1
+            if(chosenState.value + action.payload.value === 0) return
 
+            if(action.payload.type === "session") {
+
+                if(!state.isPlaying) {
+                    chosenState.value = chosenState.value + action.payload.value
+                    chosenState.runningValue = chosenState.value + action.payload.value
+                    state.displayedValue.value = chosenState.value
+                }
+                else {
+                    chosenState.value = chosenState.value + action.payload.value
+                }
+            }
+            else if (action.payload.type === "pause") {
+                chosenState.value = chosenState.value + action.payload.value 
+            }
+        }
     }
 })
 
+export const {updateChronoValues} = chrono.actions
 export default chrono.reducer
