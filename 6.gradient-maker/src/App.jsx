ef5b787db72b0a59283b957cb889f5867f4cc8d9
DirtDover <benjaminmbureau@gmail.com>
2024-01-19 17:12:34 +0100

    Gestions des inputs colors

--- 6.gradient-maker/src/App.jsx
+++ 6.gradient-maker/src/App.jsx
@@ -1,12 +1,14 @@
 import Gradient from "./components/Gradient"
 import { useSelector } from "react-redux"
+import ColorInputs from "./components/inputs/ColorInputs";
+import AddRemoveColor from "./components/AddRemoveColor.jsx";
 
 function App() {
   const gradientValues = useSelector(state => state.gradient)
-  console.log(gradientValues);
   
   
-  return <div className="relative text-slate-100 max-w-xl mx-auto mt-20 p-4 border border-slate-400 flex">
+  
+  return <div className="relative text-slate-100 max-w-4xl mx-auto mt-20 p-4 border border-slate-400 flex">
 
     <div className="w-1/2 p-4 pr-8">
         <h1 className="text-center text-xl">Gradient Generator</h1>
@@ -14,13 +16,14 @@ function App() {
     
     <p className="mb-1">Colors, min 2, max 5.</p>
     <div className="flex mb-2">
-    <input type="color" />
-      <input type="color" />
+    {gradientValues.colors.map(obj => (
+      <ColorInputs id={obj.id} color={obj.value} key={obj.id} />
+    ))}
     </div>
       
     <div className="mb-5">
-      <button>+</button>
-      <button>-</button>
+      <AddRemoveColor action={"remove"} text={"-"}/>
+      <AddRemoveColor action={"add"} text={"+"}/>
     </div>
   
   <p className="">Pick end change a Color's position</p>
--- 6.gradient-maker/src/features/gradient.js
+++ 6.gradient-maker/src/features/gradient.js
@@ -21,8 +21,26 @@ export const gradientSlice = createSlice({
   name: "gradient",
   initialState,
   reducers: {
+    updateColorValue: (state, action) => {
+        const currentColor = state.colors.find(color => color.id === action.payload.id)
+        currentColor.value = action.payload.value
+    },
+    addColor: (state, action) => {
+        if(state.colors.length === 5) return 
 
+        state.colors.push({
+            id: state.colors[state.colors.length - 1].id + 1,
+            value: "#111111",
+            position: 50
+        })
+    },
+    removeColor: (state, action) => {
+        if(state.colors.length === 2) return
+
+        state.colors.pop()
+    }
   }  
 })
 
+export const {updateColorValue, addColor, removeColor} = gradientSlice.actions
 export default gradientSlice.reducer
