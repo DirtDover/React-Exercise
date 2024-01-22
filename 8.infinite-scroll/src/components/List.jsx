bf7b9eb28d748a04ddd2ef8c7c02fff430160280
DirtDover <benjaminmbureau@gmail.com>
2024-01-22 19:34:34 +0100

    Ajout de la fonction recherche

--- 8.infinite-scroll/src/components/List.jsx
+++ 8.infinite-scroll/src/components/List.jsx
@@ -6,30 +6,70 @@ import usePhotos from '../hooks/usePhotos'
 export default function List() {
   const [query, setQuery] = useState("random")
   const [pageNumber, setPageNumber] = useState(1)
-
+  const lastPicRef = useRef()
+  const searchRef = useRef()
   const photosApiData = usePhotos(query, pageNumber)
   
+    useEffect(() => {
+
+        if(lastPicRef.current) {
+            const observer = new IntersectionObserver(([entry]) => {
+                if(entry.isIntersecting && photosApiData.maxPages !== pageNumber) {
+                    setPageNumber(pageNumber + 1)
+                    lastPicRef.current = null
+                    observer.disconnect()
+                }
+            })
+
+            observer.observe(lastPicRef.current)
+        }
+
+    }, [photosApiData])
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        if(searchRef.current.value !== query ) {
+            setQuery(searchRef.current.value)
+            setPageNumber(1)
+        }
+    }
+
     return (
     <>
        
        <h1 className="text-4xl">Unsplash Clone.</h1>
-       <form>
+       <form onSubmit={handleSubmit}>
             <label className='block mb-4' htmlFor="search">
                 Look for images...
             </label>
             <input 
+            ref={searchRef}
             placeholder='Look for something...'
             className='block w-full mb-14 text-slate-800 py-3 px-2 text-md outline-gray-500 rounded border border-slate-400'
             type="text" />
        </form>
        <ul className="grid grid-cols-[repeat(auto-fill,minmax(250px,_1fr))] auto-rows-[175px] gap-4 justify-center">
-        {!photosApiData.loader && photosApiData.photos.length !== 0 && photosApiData.photos.map((photo, index) => (
+        {!photosApiData.loader && photosApiData.photos.length !== 0 && photosApiData.photos.map((photo, index) => {
+            if(photosApiData.photos.length === index + 1) {
+                return (
+                <li 
+                ref={lastPicRef}
+                key={photo.id}>
+                    <img 
+                    className='w-full h-full object-cover'
+                    src={photo.urls.regular} alt={photo.alt_description} />
+                </li>
+                )
+            } else {
+            return (
             <li key={photo.id}>
                 <img 
                 className='w-full h-full object-cover'
                 src={photo.urls.regular} alt={photo.alt_description} />
             </li>
-        ))}
+            )    
+        }    
+})}
        </ul>
     {/*Loader */}
     {(photosApiData.loading && !photosApiData.error.state) && <img className='block mx-auto' src={spinner}/>}
--- 8.infinite-scroll/src/hooks/usePhotos.js
+++ 8.infinite-scroll/src/hooks/usePhotos.js
@@ -11,11 +11,20 @@ export default function usePhotos(querySearch, pageIndex) {
     const [loading, setLoading] = useState(true)
 
     useEffect(() => {
+        if(photos.length !== 0 && maxPages !== 0) {
+           setPhotos([])
+           setMaxPages(0) 
+        }
+    }, [querySearch])
+
+
+    useEffect(() => {
+        setLoading(true)
 
         fetch(`https://api.unsplash.com/search/photos?page=${pageIndex}&per_page=30&query=${querySearch}&client_id=${import.meta.env.VITE_UNSPLASH_KEY}`)
         .then(response => response.json())
         .then(data => {
-            setPhotos(data.results)
+            setPhotos(state => [...state, ...data.results])
             setMaxPages(data.total_pages)
             setLoading(false)
         })
