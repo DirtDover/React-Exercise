80715bb1346984423c55d0ad0e2c54ad1e64efff
DirtDover <benjaminmbureau@gmail.com>
2024-01-24 16:52:06 +0100

    Ajout du composant d'édition et de ccréation de note

--- 10.notes-app/src/App.jsx
+++ 10.notes-app/src/App.jsx
@@ -5,6 +5,7 @@ import Sidebar from "./components/Sidebar"
 import SideNotes from "./components/SideNotes"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import DisplayedNote from "./components/DisplayedNote"
+import Edit from "./components/Edit"
 
 
 function App() {
@@ -25,6 +26,8 @@ function App() {
         <Routes>
           <Route path="/" element={<NotesList />} />
           <Route path="/note/:id" element={<DisplayedNote />} />
+          <Route path="/editer" element={<Edit />} />
+          <Route path="/editer/:id" element={<Edit />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ 10.notes-app/src/components/Edit.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import {useState, useEffect} from "react"
+import { useSelector, useDispatch } from 'react-redux'
+
+export default function Edit() {
+  const [inputsStates, setInputsStates] = useState({
+    title: "",
+    subtitle: "",
+    bodyText: ""
+  })
+
+  const [showValidation, setShowValidation] = useState({
+    title: false,
+    subtitle: false,
+    bodyText: false
+  })
+  
+  
+    return (
+    <div className='w-full p-10'>
+        <p className="text-slate-100 text-xl mb-4">Ajouter une note</p>
+        
+        <form>
+            <label htmlFor="title" className='mb-2 block text-slate-100'>Le titre</label>
+            <input 
+            spellCheck="false"
+            onChange={e => setInputsStates({...inputsStates,
+            title: e.target.value})}
+            value={inputsStates.title}
+            className='p-2 text-md block w-full rounded bg-slate-200'
+            type="text"
+            id='title' />
+            {showValidation.title && (
+            <p className="text-red-400 mb-2">Veuillez renseigner un titre</p>
+        )}
+
+<label htmlFor="subtitle" className='mb-2 mt-4 block text-slate-100'>Le sous-titre</label>
+            <input 
+            spellCheck="false"
+            onChange={e => setInputsStates({...inputsStates,
+            subtitle: e.target.value})}
+            value={inputsStates.subtitle}
+            className='p-2 text-md block w-full rounded bg-slate-200'
+            type="text" 
+            id='subtitle'/>
+        {showValidation.subtitle && (
+            <p className="text-red-400 mb-2">Veuillez renseigner un sous-titre</p>
+        )}    
+        
+        <label htmlFor="bodyText" className='mb-2 mt-4 block text-slate-100'>Contenu de la note</label>
+        <textarea
+        spellCheck="false"
+        onChange={e => 
+            setInputsStates({...inputsStates, bodyText: e.target.value})}
+        value={inputsStates.bodyText}
+        id="bodytext"
+        className='w-full min-h-[300px] p2 rounded bg-slate-200'
+        ></textarea>
+        {showValidation.bodyText && (
+            <p className="text-red-400 mb-2">Veuillez écrire du contenu</p>
+        )}
+
+        <button
+        className='mt-4 px-3 py-1 rounded bg-slate-100'
+        //onClick={}
+        >Enregistrer</button>
+        </form>
+    </div>
+  )
+}
