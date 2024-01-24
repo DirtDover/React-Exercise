1eb544d49dc7c2aedcf7b7d4fc8357b52156827a
DirtDover <benjaminmbureau@gmail.com>
2024-01-24 14:00:52 +0100

    Mise en place Application

--- 10.notes-app/src/App.jsx
+++ 10.notes-app/src/App.jsx
@@ -1,5 +1,23 @@
+import NotesList from "./components/NotesList"
+import { useSelector, useDispatch } from "react-redux"
+import { getNotesFromAPI } from "./features/notes"
+
+
 function App() {
-  return <></>
+  const dispatch = useDispatch()
+  const notes = useSelector(state => state.notes)
+  console.log(notes);
+
+  if(!notes.list) {
+    dispatch(getNotesFromAPI())
+  }
+  
+  
+  
+  return <>
+
+    <NotesList />
+  </>
 }
 
 export default App
